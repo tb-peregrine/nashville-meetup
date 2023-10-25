05ec90e1d4ac690272d74880f8f05a195fb11a6e
tb-peregrine <cameronarcher@tinybird.co>
2023-10-25 13:06:29 -0500

    project

---
+++ generate_events.sh
@@ -0,0 +1,8 @@
+node_modules/@tinybirdco/mockingbird-cli/index.js confluent-cloud-kafka \
+    --schema schema.json \
+    --restEndpoint https://pkc-619z3.us-east1.gcp.confluent.cloud:443 \
+    --clusterId lkc-2j23w1 \
+    --topic nashville_demo_sales \
+    --apiKey V4GUIQH3BDX5QQ4E \
+    --apiSecret LPfnK64E5Eh95KYikCK32iZBgI2L5iQdHg6qnRo9y/aD0nMLHRMgwaxpD+OKYBrD \
+    --eps 100
